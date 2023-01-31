d9a9497f4752949cc75a07b96bbc4342bdf229ff
Michael <67141161+aim56009@users.noreply.github.com>
2023-01-31 14:47:32 +0100

    rmv import quantilemapping

--- code/src/inference.py
+++ code/src/inference.py
@@ -5,11 +5,10 @@ import torch
 from IPython.display import Image, display
 
 from Bias_GAN.code.src.model import CycleGAN, DataModule, ConstrainedGenerator
-from Bias_GAN.code.src.data import TestData, CycleDataset, ProjectionDataset,  load_cmip6_model, CMIP6Data
+from Bias_GAN.code.src.data import TestData, CycleDataset, ProjectionDataset
 from Bias_GAN.code.src.plots import PlotAnalysis
 from Bias_GAN.code.src.utils import log_transform, inv_norm_transform, inv_log_transform, inv_norm_minus1_to_plus1_transform, norm_minus1_to_plus1_transform, config_from_file
 from Bias_GAN.code.main import Config
-from Bias_GAN.code.src.quantile_mapping import QuantileMapping
 from Bias_GAN.code.src.projection_utils import ProjectionPreparation
 from Bias_GAN.code.src.xarray_utils import write_dataset
 
