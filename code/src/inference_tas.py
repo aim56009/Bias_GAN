96acc0906937d109225e086cd241b2488f5fcb8e
Michael <67141161+aim56009@users.noreply.github.com>
2023-02-09 09:24:32 +0100

    add data.py with tas instead of prec

--- code/src/inference_tas.py
+++ code/src/inference_tas.py
@@ -4,7 +4,6 @@ import torch
 from IPython.display import Image, display
 
 from Bias_GAN.code.src.model import CycleGAN, DataModule, ConstrainedGenerator
-from Bias_GAN.code.src.data import TestData, CycleDataset, ProjectionDataset
 from Bias_GAN.code.src.plots import PlotAnalysis
 from Bias_GAN.code.src.utils import log_transform, inv_norm_transform, inv_log_transform, inv_norm_minus1_to_plus1_transform, norm_minus1_to_plus1_transform, config_from_file
 
@@ -422,3 +421,234 @@ class EvaluateCheckpoints():
 def create_folder(path):
     from pathlib import Path
     Path(path).mkdir(parents=True, exist_ok=True)
+
+    
+class DataModule(pl.LightningDataModule):
+
+    def __init__(self,
+                 config,
+                 training_batch_size: int = 4,
+                 test_batch_size: int = 64):
+
+
+        super().__init__()
+
+        self.config = config
+        self.training_batch_size = training_batch_size
+        self.test_batch_size = test_batch_size
+
+    def setup(self, stage: str = None):
+
+        if stage == 'fit' or stage is None:
+            self.train = CycleDataset('train', self.config)
+            self.valid = CycleDataset('valid', self.config)
+
+        if stage == 'test':
+            self.test = CycleDataset('test', self.config)
+            self.valid = CycleDataset('valid', self.config)
+
+        if stage == 'predict':
+            self.test = ProjectionDataset(self.config)
+
+
+    def train_dataloader(self):
+        return DataLoader(self.train,
+                         batch_size=self.training_batch_size,
+                         shuffle=True,
+                         num_workers=0,
+                         pin_memory=True)
+
+
+    def val_dataloader  (self):
+        return DataLoader(self.valid,
+                          batch_size=self.test_batch_size,
+                          shuffle=False,
+                          num_workers=0,
+                          pin_memory=True)
+
+
+    def test_dataloader (self):
+        return DataLoader(self.test,
+                          batch_size=self.test_batch_size,
+                          shuffle=False,
+                          num_workers=0,
+                          pin_memory=True)
+
+
+def show_image(image):
+    plt.imshow((image.squeeze()))
+
+
+def get_random_sample(dataset):
+    return dataset[np.random.randint(0, len(dataset))]
+
+
+from dataclasses import dataclass
+import cftime
+from torch.utils.data import DataLoader
+
+
+@dataclass
+class TestData():
+    
+    era5: xr.DataArray
+    gan: xr.DataArray
+    climate_model: xr.DataArray = None
+    cmip_model: xr.DataArray = None
+    gan_constrained: xr.DataArray = None
+    poem: xr.DataArray = None
+    quantile_mapping: xr.DataArray = None
+    uuid: str = None
+    model = None
+
+
+    def model_name_definition(self, key):
+        dict = {
+            'era5': 'ERA5',
+            'gan': 'GAN (unconstrained)',
+            'climate_model': 'Climate model',
+            'cmip_model': 'GFDL-ESM4',
+            'poem': 'CM2Mc-LPJmL',
+            'gan_constrained': 'GAN',
+            'quantile_mapping': 'Quantile mapping',
+        }
+        return dict[key]
+
+
+    def colors(self, key):
+        dict = {
+            'era5': 'k',
+            'gan': 'brown',
+            'cmip_model': 'b',
+            'climate_model': 'r',
+            'gan_constrained': 'c',
+            'quantile_mapping': 'm',
+        }
+        return dict[key]
+
+        
+    def convert_units(self):
+        """ from mm/s to mm/d"""
+        self.climate_model = self.climate_model#*3600*24
+        self.era5 = self.era5#*3600*24
+        self.gan = self.gan#*3600*24
+
+    
+    def crop_test_period(self):
+        print('')
+        print(f'Test set period: {self.gan.time[0].values} - {self.gan.time[-1].values}')
+        self.climate_model = self.climate_model.sel(time=slice(self.gan.time[0], self.gan.time[-1]))
+        self.era5 = self.era5.sel(time=slice(self.gan.time[0], self.gan.time[-1]))
+
+        
+    def show_mean(self):
+        print('')
+        print(f'Mean [mm/d]:')
+        print(f'ERA5: {self.era5.mean().values:2.3f}')
+        print(f'Climate Model: {self.climate_model.mean().values:2.3f}')
+        print(f'GAN:  {self.gan.mean().values:2.3f}')
+
+
+
+
+class CycleDataset(torch.utils.data.Dataset):
+    
+    def __init__(self, stage, config, epsilon=0.0001):
+        """ 
+            stage: train, valid, test
+        """
+        self.transforms = config.transforms
+        self.epsilon = epsilon
+        self.config = config
+
+        if config.lazy:
+            self.cache = False
+            self.chunks = {'time': 1}
+        else:
+            self.cache = True
+            self.chunks = None
+
+        self.splits = {
+                "train": [str(config.train_start), str(config.train_end)],
+                "valid": [str(config.valid_start), str(config.valid_end)],
+                "test":  [str(config.test_start), str(config.test_end)],
+        }
+
+        self.stage = stage
+        self.climate_model = self.load_climate_model_data()
+        climate_model_reference = self.load_climate_model_data(is_reference=True)
+        self.era5 = self.load_era5_data()
+        era5_reference = self.load_era5_data(is_reference=True)
+        self.num_samples = len(self.era5.time.values)
+        self.era5 = self.apply_transforms(self.era5, era5_reference)
+        self.climate_model = self.apply_transforms(self.climate_model, climate_model_reference)
+
+
+    def load_climate_model_data(self, is_reference=False):
+        """ Y-domain samples """
+
+        climate_model = xr.open_dataset(self.config.poem_path,
+                                        cache=self.cache, chunks=self.chunks)
+
+        
+        climate_model =  climate_model.tas
+
+        if not self.config.lazy:
+            climate_model = climate_model.load()
+
+        if is_reference:
+            climate_model = climate_model.sel(time=slice(self.splits['train'][0],
+                                                         self.splits['train'][1]))
+        else:
+            climate_model = climate_model.sel(time=slice(self.splits[self.stage][0],
+                                                         self.splits[self.stage][1]))
+
+        return climate_model
+
+
+    def load_era5_data(self, is_reference=False):
+        """ X-domain samples """
+
+        era5 = xr.open_dataset(self.config.era5_path,
+                               cache=self.cache, chunks=self.chunks)\
+                               .tas
+
+        if not self.config.lazy:
+            era5 = era5.load()
+
+        if is_reference:
+            era5 = era5.sel(time=slice(self.splits['train'][0],
+                                       self.splits['train'][1]))
+        else:
+            era5 = era5.sel(time=slice(self.splits[self.stage][0],
+                                 self.splits[self.stage][1]))
+
+        return era5
+        
+
+    def apply_transforms(self, data, data_ref):
+
+        if 'log' in self.transforms:
+            data = log_transform(data, self.epsilon)
+            data_ref = log_transform(data_ref, self.epsilon)
+
+        if 'normalize' in self.transforms:
+            data = norm_transform(data, data_ref)
+
+        if 'normalize_minus1_to_plus1' in self.transforms:
+            data = norm_minus1_to_plus1_transform(data, data_ref)
+        
+        return data
+
+
+    def __getitem__(self, index):
+
+        x = torch.from_numpy(self.era5.isel(time=index).values).float().unsqueeze(0)
+        y = torch.from_numpy(self.climate_model.isel(time=index).values).float().unsqueeze(0)
+
+        sample = {'A': x, 'B': y}
+        
+        return sample
+
+    def __len__(self):
+        return self.num_samples
